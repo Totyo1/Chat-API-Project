50a1a7556cb9d55bc3379706d24e2c802e01bc69
Tod Totev <t.totev@edynamix.com>
2020-02-05 09:12:56 +0000

    Models , service and data layers (empty :))

---
+++ ChatAPIProject/ChatAPIProject/Models/InputModels/CommunicationInputModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatAPIProject.Models.InputModels
+{
+    public class CommunicationInputModel
+    {
+    }
+}
