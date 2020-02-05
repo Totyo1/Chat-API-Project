af3191d6fa854fede0badc93cfcccbb73aa45324
Georgi Horozov <g.horozov@edynamix.com>
2020-02-05 11:12:04 +0000

    Add methods to services

---
+++ ChatAPIProject/Models/ServiceModels/FriendRequest/FriendRequestServiceModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.ServiceModels.FriendRequest
+{
+    public class FriendRequestServiceModel
+    {
+        public int SenderId { get; set; }
+
+        public int ReceiverId { get; set; }
+
+        public string Status { get; set; }
+    }
+}
--- ChatAPIProject/Servise/FriendRequestSevice.cs
+++ ChatAPIProject/Servise/FriendRequestSevice.cs
@@ -1,13 +1,30 @@
-﻿using Servise.Contracts;
+﻿using Models.ServiceModels.FriendRequest;
+using Servise.Contracts;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Servise
 {
     public class FriendRequestSevice : IFriendRequestSevice
     {
+        public IEnumerable<FriendRequestServiceModel> All()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ApproveRequest(int senderId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeclineRequest(int senderId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SendFriendRequest(int receiverId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
