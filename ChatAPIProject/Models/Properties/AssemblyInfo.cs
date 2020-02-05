d1764454728b072c1dbdd01e60dcf049a28791b6
Tod Totev <t.totev@edynamix.com>
2020-02-05 09:42:55 +0000

    new order

--- ChatAPIProject/Models/Properties/AssemblyInfo.cs
+++ ChatAPIProject/Models/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Data")]
+[assembly: AssemblyTitle("Models")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Data")]
+[assembly: AssemblyProduct("Models")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ebc59297-d158-42d1-9acb-e0beecedbd4d")]
+[assembly: Guid("01925568-ba84-4307-873a-3d75b9054b6f")]
 
 // Version information for an assembly consists of the following four values:
 //
--- ChatAPIProject/Servise/Properties/AssemblyInfo.cs
+++ ChatAPIProject/Servise/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Data")]
+[assembly: AssemblyTitle("Servise")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Data")]
+[assembly: AssemblyProduct("Servise")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ebc59297-d158-42d1-9acb-e0beecedbd4d")]
+[assembly: Guid("1089bbdb-d1e4-4731-a24e-eff3e7b990fa")]
 
 // Version information for an assembly consists of the following four values:
 //
